7999b481d043b884f605bc61f6d4f5c1211a1223
Rahul-Selvaraju <46060543+Rahul-Selvaraju@users.noreply.github.com>
2019-05-30 16:25:54 +0530

    Update prime or not .py

--- prime or not .py
+++ prime or not .py
@@ -2,7 +2,7 @@ a=int(input())
 if a > 1: 
     for i in range(2, a//2):
         if (a % i) == 0:
-            print(a, "No")
+            print(a, "no")
             break
     else:
         print(a, "yes") 
