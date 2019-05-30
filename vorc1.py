68d3b94eacb8c35a59c82739006a1e2f1b44b122
Rahul-Selvaraju <46060543+Rahul-Selvaraju@users.noreply.github.com>
2019-05-30 10:23:46 +0530

    Add files via upload

---
+++ vorc1.py
@@ -0,0 +1,9 @@
+a=input()
+b=['a','e','i','o','u']
+c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if(a in b):
+    print("Vowel")
+elif(a in c):
+    print("Consonant")
+else:
+    print("invalid")
